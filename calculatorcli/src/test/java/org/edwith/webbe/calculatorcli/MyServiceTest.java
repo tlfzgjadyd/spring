946ce94ca64aa54beea4760ab547007d31f2ecdf
tlfzgjadyd <tlfzgjadyd@naver.com>
2020-07-22 16:55:51 +0900

    DBConfig dao dto db연동및 메인테스트

--- calculatorcli/src/test/java/org/edwith/webbe/calculatorcli/MyServiceTest.java
+++ calculatorcli/src/test/java/org/edwith/webbe/calculatorcli/MyServiceTest.java
@@ -28,7 +28,8 @@ public class MyServiceTest {
 		int value1=5;
 		int value2=10;
 		given(calculatorService.plus(anyInt(), anyInt())).willReturn(15);
-							//예제와 다르게 given측에도 anyInt를 통해 호출해야만 의도한대로 뭔수를 넣든 15란 결과가 나와서 +2해서 리턴되어 17이 나온다
+							//예제와 다르게 given측에도 anyInt를 통해 호출해야만
+							//의도한대로 뭔수를 넣든 15란 결과가 나와서 +2해서 리턴되어 17이 나온다
 		//when	
 		int result= myService.execute(value1, value2);
 		
